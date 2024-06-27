15ec51f2fad991f7487a07a97b84417ab9030a69
mark zhong <417498134@qq.com>
2024-06-27 15:27:07 +1200

    Update main.py

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ if __name__ == "__main__":
     ###############################
     print("Save Results")
     for image_i, o in enumerate(X):
+        image_i =image_i.cpu().numpy()
         o = o.cpu().numpy()
         o = np.array(o * 255).astype(np.uint8)
         img_o = Image.fromarray(o.transpose(1, 2, 0), mode="RGB")
